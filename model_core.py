80c0083952912ec125f84c916d9136125d0d8b88
leuchthelp <gfpinthehouse@gmail.com>
2026-02-12 22:16:44 +0000

    Allow model to be downloaded on startup
    Former-commit-id: 926a10ee0b80a86626baeefd20530868d90e3262

--- model_core.py
+++ model_core.py
@@ -3,7 +3,7 @@ from colorama import Fore
 import logging
 import os 
 
-os.environ['TRANSFORMERS_OFFLINE'] = '1' 
+#os.environ['TRANSFORMERS_OFFLINE'] = '1' 
 os.environ['HF_HUB_DISABLE_TELEMETRY'] = '1'
 
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
