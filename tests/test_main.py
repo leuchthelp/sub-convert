2dfda76e374a2303c9d866f572c9208c7450f029
leucht <gfpinthehouse@gmail.com>
2026-04-19 17:39:49 +0000

    check something

--- tests/test_main.py
+++ tests/test_main.py
@@ -16,14 +16,14 @@ from main import (
 def test_check_aged_pos_offset():
     path = Path("tests/files/for-main/test-adjacent-exists.srt")
     assert check_aged(path, offset="s+1") is True
-    assert check_aged(path, offset="+1") is True
+    #assert check_aged(path, offset="+1") is True
     assert check_aged(path, offset="1") is True
 
 
 def test_check_aged_neg_offset():
     path = Path("tests/files/for-main/test-adjacent-exists.srt")
     assert check_aged(path, offset="s-1") is False
-    assert check_aged(path, offset="-1") is False
+    #assert check_aged(path, offset="-1") is False
 
 
 @pytest.mark.parametrize(
